a25b8c6a4799486af510d67671b7b87b323a04f3
NoOneHardy <silas.hardegger@outlook.com>
2023-09-13 16:30:21 +0200

    Tag 1

---
+++ tag1/01_script.js
@@ -0,0 +1,16 @@
+let firstName = "Hans";
+let x = 6;
+let u = firstName + x;
+console.log(`Hallo ${u}!`);
+
+console.info("Info");
+console.warn("Warning");
+console.error("Error");
+
+test('bestanden');
+function test(a) {
+    document.getElementById('text1').innerHTML = `Test: ${a}`;
+}
+
+var data = ['Hans', 'Peter', 'Paul'];
+console.log(data);
